d274adfba418d1353c3c67e8efb60eb2b2972509
6431503077 <6431503077@lamduan.mfu.ac.th>
2025-05-19 13:31:48 +0700

    Debug error deployment

--- app/pos/(overview)/page.tsx
+++ app/pos/(overview)/page.tsx
@@ -1,53 +1,28 @@
 
 import CardList from "../../ui/pos/card-list";
-import Search from "@/app/ui/pos/search";
-import Pagination from "@/app/ui/pos/pagination";
-import { fetchPosPages,fetchPosTotal } from "@/app/lib/pos/data";
-import CurrentDate from "@/app/ui/current-date";
 import CheckoutCard from "@/app/ui/pos/cart-card";
 import { Suspense } from "react";
 
-function TotalItem({ totalItems }: { totalItems: number }) {
-  return (
-    <div className="flex justify-center">
-      <p>Item: {totalItems}</p>
-    </div>
-  );
-}
-
-export default async function PosPage(props: {
-  searchParams?: Promise<{
-      query?: string;
-      page?: string;
-  }>;
+export default async function PosPage({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
 }) {
-  const searchParams = await props.searchParams;
-  const query = searchParams?.query || '';
-  const currentPage = Number(searchParams?.page) || 1;
-  const totalPages = await fetchPosPages(query);
-  const totalItems = await fetchPosTotal(query);
-
   return (
-     <main className="grid grid-cols-5">
-        <div className="col-span-5 flex gap-2 justify-center items-center">
-          <Search placeholder={"..."} />
-          <CurrentDate />
-        </div>
-        <div className="col-span-5 flex justify-between">
-          <TotalItem totalItems={totalItems} />
-          <Pagination totalPages={totalPages} />
-        </div>
-        <div className="col-span-3">
-          <CardList query={query} currentPage={currentPage} />
-        </div>
-        <div className="col-span-2 flex flex-col">
-          <Suspense fallback={<div>Loading</div>}>
-            <CheckoutCard />
-          </Suspense>
-
-        </div>
-       
-      </main>
+    <main className="flex flex-col lg:flex-row justify-between">
+      <Suspense fallback={<div>Loading products...</div>}>
+        <CardList searchParams={searchParams} />
+      </Suspense>
+      
+      <div className="col-span-2 flex flex-col">
+        <Suspense fallback={<div>Loading cart...</div>}>
+          <CheckoutCard />
+        </Suspense>
+      </div>
+    </main>
   );
 }
 
--- app/ui/pos/card-list.tsx
+++ app/ui/pos/card-list.tsx
@@ -1,31 +1,57 @@
 
 
 import Card from './card';
-import { fetchFilteredItems } from "../../lib/pos/data";
+import { fetchFilteredItems, fetchPosPages } from "../../lib/pos/data";
+import Search from "@/app/ui/pos/search";
+import Pagination from "@/app/ui/pos/pagination";
+import { fetchPosTotal } from '../../lib/pos/data';
+
+
+function TotalItem({ totalItems }: { totalItems: number }) {
+  return (
+    <div className="flex justify-center">
+      <p>Item: {totalItems}</p>
+    </div>
+  );
+}
 
 export default async function CardList({
-    query,
-    currentPage,
-  }: {
-    query: string;
-    currentPage: number;
-  }) {
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+    const query = searchParams?.query || '';
+    const currentPage = Number(searchParams?.page) || 1;
+    const totalPages = await fetchPosPages(query);
+    const totalItems = await fetchPosTotal(query);
     const items = await fetchFilteredItems(query, currentPage);
     
     return (
-        <div className="flex flex-wrap gap-6">
-          {items.map((item) => (
-            <Card
-              key={item.no}
-              no={item.no}
-              productId={item.productId}
-              productName={item.productName}
-              category={item.category}
-              price={item.price}
-              imageUrl={item.imageUrl}
-              stock={item.stock}
-            />
-          ))}
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 h-full hidden-lg:w-full lg:w-[66.666667%] overflow-hidden lg:sticky lg:top-0">
+        <div className='lg:col-span-3 grid-cols-2 flex flex-col'>
+          <Search placeholder={"..."} />
+            
+          <div className="col-span-5 flex justify-between">
+            <TotalItem totalItems={totalItems} />
+            <Pagination totalPages={totalPages} />
+          </div>
         </div>
+          
+        {items.map((item) => (
+          <Card
+            key={item.no}
+            no={item.no}
+            productId={item.productId}
+            productName={item.productName}
+            category={item.category}
+            price={item.price}
+            imageUrl={item.imageUrl}
+            stock={item.stock}
+          />
+        ))}
+      </div>
     );
-  }
+}
